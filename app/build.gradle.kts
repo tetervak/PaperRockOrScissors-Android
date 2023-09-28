1c16c9b2ae083db23b9d1f806a8901ddd186a0e1
Alex Tetervak <tetervak@sheridancollege.ca>
2023-09-28 12:40:31 -0400

    added navigation with compose library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.44.2")
     kapt("com.google.dagger:hilt-android-compiler:2.44.2")
 
+    val nav_version = "2.7.3"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
